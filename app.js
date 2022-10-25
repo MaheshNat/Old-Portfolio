0b7d94846a77ad09fdac13d74d75ff25ae7372b3
MaheshNat <maheshkumar.natamai@gmail.com>
2022-10-25 18:25:08 +0000

    remove /api/ prefix from all routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const axios = require('axios');
 const CronJob = require('cron').CronJob;
 const Episode = require('./models/Episode');
 
-const PORT = process.env.PORT || 8080;
+const PORT = "3000";
 
 const app = express();
 const contactRouter = require('./routes/contact');
@@ -32,29 +32,22 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
 app.use(sslRedirect());
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use('/api/contact', contactRouter);
-app.use('/api/projects', projectsRouter);
-app.use('/api/update-resume', updateResumeRouter);
-app.use('/api/episodes', (req, res) => {
+app.use('/contact', contactRouter);
+app.use('/projects', projectsRouter);
+app.use('/update-resume', updateResumeRouter);
+app.use('/episodes', (req, res) => {
   Episode.find({}).then((episodes) => {
     res.json(episodes);
   });
 });
 
-app.use('/api/resume', (req, res) => {
+app.use('/resume', (req, res) => {
   res.download('./assets/mahesh-natamai-resume.pdf');
 });
 
-app.use('/api/resume-file', (req, res) => {
+app.use('/resume-file', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'assets', 'mahesh-natamai-resume.pdf'));
 });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res, next) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 //Connect To Database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
